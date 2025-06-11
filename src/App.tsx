0a8ce10cfb5957122e43991fc1622ac8019acfdc
unknown <kikialex37@gmail.com>
2025-06-11 20:48:29 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import DisplayWeather from './Components/DisplayWeather'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <DisplayWeather/>
+    </div>
+  )
+}
+
+export default App
